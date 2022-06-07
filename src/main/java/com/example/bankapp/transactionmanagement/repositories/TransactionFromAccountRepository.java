15f53310e790ba712b81e0e72b601fb11a4c8432
Dilara Bülbül <97522259+DBulbul@users.noreply.github.com>
2022-06-07 10:40:32 +0300

    Update TransactionFromAccountRepository.java

--- src/main/java/com/example/bankapp/transactionmanagement/repositories/TransactionFromAccountRepository.java
+++ src/main/java/com/example/bankapp/transactionmanagement/repositories/TransactionFromAccountRepository.java
@@ -12,5 +12,5 @@ import java.util.List;
 
 @Repository
 public interface TransactionFromAccountRepository extends JpaRepository<TransactionFromAccount,Long> {
-    TransactionFromAccount findByIBAN(long IBAN);
+
 }
